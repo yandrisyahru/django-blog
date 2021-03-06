ac4e4bf62af93cd9922b023fb2545c7ba8d7d78d
Yandri Syahru Ramayahya <yandrisyahru95@gmail.com>
2021-03-06 23:07:23 +0700

    Menampilkan Model ke Home Page

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from myblog import views
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('blog/', views.blog, name='blog'),
+    path('tinymce/', include('tinymce.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
