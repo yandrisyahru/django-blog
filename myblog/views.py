3c13a246a62cfd4f404e7fb57b6dd70a4282272a
Yandri Syahru Ramayahya <yandrisyahru95@gmail.com>
2021-03-07 21:35:19 +0700

    Cripsy Form

--- myblog/views.py
+++ myblog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     queryset = Post.objects.all()
+    paginator = Paginator(queryset, 2)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
     context = {
-        'queryset' : queryset
+        'queryset' : paginated_queryset,
+        'page_request_var' : page_request_var
     }
     return render(request, 'index.html', context)
 
